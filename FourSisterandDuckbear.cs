1a1f8a9546eaf1d9909257e81a891e902be8aeb2
Shrijan Paudel <102872515+SrijanPoudel@users.noreply.github.com>
2025-02-01 14:37:11 -0600

    Four Sister and Duckbear

---
+++ FourSisterandDuckbear.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Four_Sisters_and_the_Duckbear
+{
+    public class FourSisterandDuckbear
+    {
+        private int eggs = 0;
+
+
+
+
+        public int Eggs
+        {
+            get { return this.eggs; }
+            set
+            {
+                if (value > 0)
+                {
+                    this.eggs = value;
+                }
+                else
+                {
+                    this.eggs = 0;
+                }
+            }
+        }
+        public int EqualDivided
+        {
+            get { return this.eggs / 4; }
+        }
+        public int RemainingDivided
+        {
+            get { return this.eggs % 4; }
+        }
+
+        public string GetResultMessage()
+        {
+            int equalshare = EqualDivided;
+            int leftover = RemainingDivided;
+
+            if (leftover == 0)
+            {
+                return ($"There are no leftover eggs. Each sister will get {equalshare} eggs.");
+            }
+            else
+            {
+                return ($"If there are {eggs} eggs, each sister gets {equalshare} eggs, and there are {leftover} leftover eggs.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Written by Shrijan Paudel
+//Date : 01/26
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Four_Sisters_and_the_Duckbear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FourSisterandDuckbear calculator = new FourSisterandDuckbear();
+            Console.WriteLine("Welcome to the Four Sisters and the Duckbear program!");
+
+            Console.WriteLine("Enter Number of Eggs:");
+            int numeggs = Convert.ToInt32(Console.ReadLine());
+
+            if (numeggs > 0)
+            {
+                calculator.Eggs = numeggs;
+                Console.WriteLine(calculator.GetResultMessage());
+            
+            }
+        }
+    }
+}
